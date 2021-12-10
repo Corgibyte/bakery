0b3b957e11c8537e50d31da0fd56c5bb822c8b85
Hannah Young <hannah@corgibyte.com>
2021-12-10 12:47:37 -0800

    Implement GetTotal; all tests pass

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Bakery.Models
@@ -30,8 +31,33 @@ namespace Bakery.Models
 
     public int GetTotal()
     {
-      //TODO: implement
-      return -1;
+      //Create dictionary connecting types to functions to calculate cost
+      Dictionary<Type, Func<int, int>> priceFunctions = new Dictionary<Type, Func<int, int>>
+        {
+          {typeof(Bread), (int x) => 5 * (x - (x / 3)) },
+          {typeof(Pastry), (int x) => 2 * (x - (x / 3)) + 1 * (x / 3) }
+        };
+      //Create a new dictionary to hold total number of each type
+      Dictionary<Type, int> counts = new Dictionary<Type, int> { };
+      //Add or increment by 1 for each type in list
+      foreach (Orderable item in _orderItems)
+      {
+        if (!counts.ContainsKey(item.GetType()))
+        {
+          counts[item.GetType()] = 1;
+        }
+        else
+        {
+          counts[item.GetType()]++;
+        }
+      }
+      //Find cost of each type of orderable and add to sum
+      int total = 0;
+      foreach (KeyValuePair<Type, int> count in counts)
+      {
+        total += priceFunctions[count.Key](count.Value);
+      }
+      return total;
     }
 
     public override string ToString()
