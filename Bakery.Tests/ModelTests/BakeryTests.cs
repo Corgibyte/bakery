622ee36bedea8420744e4c6de641418673ae7420
Hannah Young <hannah@corgibyte.com>
2021-12-10 09:22:20 -0800

    Add initial spec and tests

---
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
