ec1d531c5405f1b2fc39eec936e8327d7bafe515
Hannah Young <hannah@corgibyte.com>
2021-12-10 10:13:13 -0800

    Add test for description; tests pass

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -14,6 +14,15 @@ namespace Bakery.Tests
       Bread newBread = new(test);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_DescriptionString()
+    {
+      string description = "Brioche";
+      Bread testBread = new(description);
+      string result = testBread.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 
   [TestClass]
