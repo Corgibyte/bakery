14b1c666e6d11ca4453d3d8ba208ee9638c6098a
Hannah Young <hannah@corgibyte.com>
2021-12-10 10:31:53 -0800

    Add Order with initial spec

---
+++ Bakery/Models/Order.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Order
+  {
+    public Order()
+    {
+      //TODO: implement constructor
+    }
+
+    public void AddItem(Orderable item)
+    {
+      //TODO: implement
+    }
+
+    public void ClearAll()
+    {
+      //TODO: implement
+    }
+
+    public int GetTotal()
+    {
+      //TODO: implement
+      return -1;
+    }
+
+    public override string ToString()
+    {
+      //TODO: implement
+      return "a";
+    }
+  }
+}
